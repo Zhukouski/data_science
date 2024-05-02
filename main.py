e308c3e7ca46e29544d183a791e327625fefca88
Zhukouski <r.zhukouski@gmail.com>
2024-05-02 20:30:45 +0300

    Обучение Data science

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#словарь
+students = {
+    'студент1': {'имя': 'Роман', 'возраст': 31, 'курс': 3},
+    'студент2': {'имя': 'Игорь', 'возраст': 50, 'курс': 4},
+    'студент3': {'имя': 'Иван', 'возраст': 44, 'курс': 1}
+}
+print("students:", students)
